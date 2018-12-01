f19a9ab98f51712dc4d4171299ea4dc2ded55341
dprovodnikov <wireden@yandex.ru>
2018-12-01 11:12:29 +0200

    Add ignore files

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+import { actionCreator } from '../src';
+
+describe('actionCreator util test suit', () => {
+  test('is a function', () => {
+    expect(actionCreator).toBeInstanceOf(Function);
+  });
+
+  test('returns a function', () => {
+    expect(actionCreator()).toBeInstanceOf(Function);
+  });
+
+  test('composes an action creator that works correctly', () => {
+    const output = actionCreator('TYPE')('PAYLOAD');
+    const expectedOutput = { type: 'TYPE', payload: 'PAYLOAD' };
+    expect(output).toEqual(expectedOutput);
+  });
+});
